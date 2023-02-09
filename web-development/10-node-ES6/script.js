cc5b2e31f2b1afb1ef2247dd94c765041e8dcb08
heispv <peymanvahidi1998@gmail.com>
2023-02-09 16:22:17 +0330

    using top await

--- web-development/10-node-ES6/script.js
+++ web-development/10-node-ES6/script.js
@@ -1,7 +1,12 @@
 // const script2 = require('./script2.js')
-import { largeNumber } from "./script2.js";
+// import { largeNumber } from "./script2.js";
 
-const a = largeNumber;
+let a;
+if (true) {
+  const { largeNumber } = await import('./script2.js')
+  a = largeNumber;
+}
+
 const b = 5;
 
-console.log(a + b);
+console.log(a, b);
