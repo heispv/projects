0e0bdd671d1c1c0ef452199f76e07a74773a0ebc
heispv <peymanvahidi1998@gmail.com>
2023-02-13 14:55:43 +0330

    using postman

--- web-development/11-express/server.js
+++ web-development/11-express/server.js
@@ -2,20 +2,19 @@ import express from "express";
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log("<h1>Hello</h1>");
-  next();
-})
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send("<h1>we are in root</h1>")
+  res.send("<h1>we are in root</h1>");
 })
 
 app.get('/profile', (req, res) => {
-  const user = {
-    name: "Sally",
-    age: 5
-  }
-  res.send(user)
+  res.send("getting profile");
 })
-app.listen(3000)
+
+app.post('/profile', (req, res) => {
+  console.log(req.body);
+  res.send('Success');
+})
+app.listen(3000);
