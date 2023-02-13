c9fdd618d5b8a5a9e9403b9d456acc6c078ec2a9
heispv <peymanvahidi1998@gmail.com>
2023-02-13 11:29:16 +0330

    modified files

---
+++ web-development/10-node-ES6/server.js
@@ -0,0 +1,16 @@
+import http from "http";
+
+const server = http.createServer((request, response) => {
+  // console.log("headers ->", request.headers);
+  console.log("method ->", request.method);
+  console.log("url ->", request.url);
+  const user = {
+    name: "john",
+    hobby: "skating"
+  }
+
+  response.setHeader("Content-Type", "application/json");
+  response.end(JSON.stringify(user));
+})
+
+server.listen(3001);
