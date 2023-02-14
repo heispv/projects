ea5a1daca6405c5027aa95b55c7717b5bd2b6521
heispv <peymanvahidi1998@gmail.com>
2023-02-14 16:37:03 +0330

    add file - signin & register

---
+++ web-development/15-smart-brain-api/server.js
@@ -0,0 +1,66 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Pey',
+      email: 'pey@gmail.com',
+      password: 'hellosally',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email &&
+      req.body.password === database.users[0].password) {
+        res.json('success');
+      } else {
+        res.status(400).json('error logging in');
+      }
+})
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '123',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(database.users[database.users.length-1]);
+})
+
+app.listen(3000, () => {
+  console.log('app is running on port 3000');
+})
+
+
+/*
+
+/                 => res = this is working
+/signin           => POST = success/fail
+/register         => POST = user
+/profile/:userId  => GET = user
+/image            => PUT = user
+
+*/
