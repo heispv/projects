7e611506fa678286c0da3a29b7852cf7e30c9af2
heispv <peymanvahidi1998@gmail.com>
2023-02-18 17:09:33 +0330

    adding database to the server

---
+++ web-development/18-server-database/server.js
@@ -0,0 +1,100 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import cors from 'cors';
+import knex from 'knex';
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'test',
+      database : 'smart-brain'
+    }
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const is_valid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (is_valid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(password, salt);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+       trx.insert({
+          email: loginEmail[0].email,
+          name: name,
+          joined: new Date()
+        })
+        .into('users')
+        .returning('*')
+        .then(user => {
+          res.json(user[0]);
+        })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+  .catch(err => res.status(400).json('Unable to register !'))
+})
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({
+    id: id
+  }).then(user => {
+    if (user.length) {
+      res.json(user[0])
+    } else {
+      res.status(400).json('Not found')
+    }
+  })
+  .catch(err => res.status(400).json('error getting user'))
+})
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+})
+
+app.listen(3001, () => {
+  console.log('app is running on port 3001');
+})
