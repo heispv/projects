4b19e9073e7d5c4614efbb8112bcf6ba7ccb756d
heispv <peymanvahidi1998@gmail.com>
2023-02-19 15:49:59 +0330

    adding controllers folder to have a cleaner server.js code

---
+++ web-development/18-server-database/controllers/register.js
@@ -0,0 +1,30 @@
+const handleRegister = (req, res, db, bcrypt) => {
+  const { email, name, password } = req.body;
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(password, salt);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+       trx.insert({
+          email: loginEmail[0].email,
+          name: name,
+          joined: new Date()
+        })
+        .into('users')
+        .returning('*')
+        .then(user => {
+          res.json(user[0]);
+        })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+  .catch(err => res.status(400).json('Unable to register !'))
+}
+
+export default handleRegister;
