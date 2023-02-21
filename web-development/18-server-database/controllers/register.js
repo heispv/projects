18c3f9212481b44be4c0f1dc5f847f11fddd7928
heispv <peymanvahidi1998@gmail.com>
2023-02-21 13:40:34 +0330

    imporving security

--- web-development/18-server-database/controllers/register.js
+++ web-development/18-server-database/controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('none of the fields should be blank')
+  }
   var salt = bcrypt.genSaltSync(10);
   var hash = bcrypt.hashSync(password, salt);
   db.transaction(trx => {
--- web-development/18-server-database/controllers/signin.js
+++ web-development/18-server-database/controllers/signin.js
@@ -1,11 +1,15 @@
 const handleSignin = (req, res, db, bcrypt) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json('none of the fields should be blank')
+  }
   db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
+    .where('email', '=', email)
     .then(data => {
-      const is_valid = bcrypt.compareSync(req.body.password, data[0].hash);
+      const is_valid = bcrypt.compareSync(password, data[0].hash);
       if (is_valid) {
         return db.select('*').from('users')
-          .where('email', '=', req.body.email)
+          .where('email', '=', email)
           .then(user => {
             res.json(user[0])
           })
